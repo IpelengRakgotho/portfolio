21b5627cdd8c830c3f5aa7e03a190a73628c3b4f
Lerato <lerato.rakgotho@gmail.com>
2026-06-16 18:17:01 +0200

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿//const toggleBtn = document.getElementById("theme-toggle");
+
+//toggleBtn.addEventListener("click", () => {
+//    document.body.classList.toggle("dark-mode");
+
+//    toggleBtn.textContent =
+//        document.body.classList.contains("dark-mode")
+//            ? "☀️"
+//            : "🌙";
+//});
